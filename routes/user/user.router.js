216b6b422c0ac740ba1d72b920b322c6729df2db
David Castejon <ombengue@hotmail.com>
2021-07-18 13:02:50 +0200

    Fix integration between front and backend when adding products to the cart

--- routes/user/user.router.js
+++ routes/user/user.router.js
@@ -3,31 +3,13 @@ const userRouter = express.Router();
 
 const User = require('./../../models/User.model');
 
-userRouter.put("/:id/addToCart", (req, res, next) => {
+userRouter.put("/:id/modifyCart", (req, res, next) => {
     const { id } = req.params;
-    const { product } = req.body;
+    const { cartState } = req.body;
 
-    console.log(product)
-
-    /*User.findById(id)
-        .then((user) => {
-            for (const item of user.productsInCart) {
-                if (Object.keys(item)[0] === product) {
-                    console.log("adding one!");
-                    break;
-                }
-                else {
-                    return User.findByIdAndUpdate(id, {$push: {productsInCart: {[product]: 1}}});
-                }
-            }
-        })
-        .then(() => res.status(201))
+    User.findByIdAndUpdate(id, { productsInCart: cartState })
+        .then(() => res.status(200))
         .catch((err) => next(err));
-
-
-    /* User.findByIdAndUpdate(id, {$push: {productsInCart: {[product]: 1}}})
-        .then(() => res.status(201))
-        .catch((err) => next(err)); */
 });
 
 userRouter.put("/:id/incrementLogin", (req, res, next) => {
